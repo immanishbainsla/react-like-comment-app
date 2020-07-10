31812176face4427a858796e0438be97e9f5ad3f
immanishbainsla <manishbainsla1072@gmail.com>
2020-07-10 22:15:09 +0530

    Updates

---
+++ precache-manifest.991138716c65d0f5aa987b7166ec78f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e29489af82d71f112d0d19066088c33",
+    "url": "/react-like-comment-app/index.html"
+  },
+  {
+    "revision": "07d9aba8847adf0eb69e",
+    "url": "/react-like-comment-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c4546463da87dd49f0e5",
+    "url": "/react-like-comment-app/static/js/2.8e5aedf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-like-comment-app/static/js/2.8e5aedf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d9aba8847adf0eb69e",
+    "url": "/react-like-comment-app/static/js/main.90cfe410.chunk.js"
+  },
+  {
+    "revision": "4a8420ce3fca4140c317",
+    "url": "/react-like-comment-app/static/js/runtime-main.f9e798de.js"
+  }
+]);
